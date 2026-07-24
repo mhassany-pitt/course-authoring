7a6eff4622e4c17ea93dcadb7da6bb1a258892f9
Mohammad Hassany <moh70@pitt.edu>
2026-07-24 18:40:29 -0400

    fix: enhance type safety and session cookie settings in main application bootstrap

--- authoring-api/src/main.ts
+++ authoring-api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import session from 'express-session';
@@ -10,8 +11,9 @@ import { corsMiddleware } from './cors-middleware';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
+  app.set('trust proxy', 1);
 
   const config = app.get(ConfigService);
   const production = (process.env.NODE_ENV || 'development').toLowerCase() == 'production';
@@ -31,6 +33,7 @@ async function bootstrap() {
     store: new (FileStore(session))({ path: config.get('STORAGE_PATH') + '/sessions' }),
     cookie: {
       secure: production,
+      sameSite: 'lax',
       maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     }
   }))
